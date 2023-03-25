7145484f28a64c01ac527b53fb8dababa35c15fe
Devashish Deshmukh <devashishadeshmukh@gmail.com>
2023-03-25 23:53:58 +0530

    login and signup and search functions added

--- backend/data/data.js
+++ backend/data/data.js
@@ -1,19 +1,19 @@
 const chats = [
-    // {
-    //   isGroupChat: false,
-    //   users: [
-    //     {
-    //       name: "John Doe",
-    //       email: "john@example.com",
-    //     },
-    //     {
-    //       name: "Piyush",
-    //       email: "piyush@example.com",
-    //     },
-    //   ],
-    //   _id: "617a077e18c25468bc7c4dd4",
-    //   chatName: "John Doe",
-    // },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "John Doe",
+          email: "john@example.com",
+        },
+        {
+          name: "Piyush",
+          email: "piyush@example.com",
+        },
+      ],
+      _id: "617a077e18c25468bc7c4dd4",
+      chatName: "John Doe",
+    },
     {
       isGroupChat: false,
       users: [
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,17 @@ const express=require("express");
 const connectDB = require("./config/db");
 const {chats} = require("./data/data");
 require('dotenv').config()
-
+const chatRoutes = require("./routes/chatRoutes")
+const userRoutes = require("./routes/userRoutes")
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app =express();
 connectDB();
+
+app.use( 
+    express.urlencoded({extended:true})
+);
+app.use(express.json()); // to accept JSON Data
 
 //USE POSTMAN FOR TESTING THE APIS 
 //Just to check if api is working 
@@ -13,18 +20,27 @@ app.get('/', (req,res)=>{
     res.send("API is working");
 });
 
-//sends all chats at once in  form of javascript object 
-app.get('/api/chat', (req, res)=>{
-    res.send(chats);
-});
+// //sends all chats at once in  form of javascript object 
+// app.get('/api/chat', (req, res)=>{
+//     res.send(chats);
+// });
 
 
-//sends a chats whose ID is given form of javascript object 
-app.get('/api/chat/:id', (req, res)=>{
-    // console.log(req.params.id);
-    const singleChat = chats.find((c)=> c._id === req.params.id);
-    res.send(singleChat);
-});
+// //sends a chats whose ID is given form of javascript object 
+// app.get('/api/chat/:id', (req, res)=>{
+//     // console.log(req.params.id);
+//     const singleChat = chats.find((c)=> c._id === req.params.id);
+//     res.send(singleChat);
+// });
+
+
+app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
+
+
+app.use(notFound);
+app.use(errorHandler);  
+
 
 const PORT =process.env.PORT || 5000
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
